5edf9e21349b9ff12c1a33acec4c50c5da7f46ef
HOUETOR <bopiflo05@gmail.com>
2026-08-09 00:26:12 +0100

    fix: imports CSS de la maquette (chemins relatifs pages)

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { HashRouter } from "react-router-dom"
 import "./styles.css"
-import "./LandingPage.css"
-import "./LegalPage.css"
 import "./index.css"
 import App from "./App.tsx"
 
